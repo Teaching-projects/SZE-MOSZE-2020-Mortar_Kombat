f4ff31522efc224e7e514e0646ac1f089b5a5fa8
balazs482 <balazs482@gmail.com>
2020-09-22 01:18:23 +0200

    New winner announcement
    New winner announcement wording, adjusted README

--- main.cpp
+++ main.cpp
@@ -3,42 +3,42 @@
 
 int main(const int argc, const char** argv) {
 
-    if (argc == 7)
-    {
-        Character player1 = Character(argv[1], std::stoul(argv[2]), std::stoul(argv[3]));
-        Character player2 = Character(argv[4], std::stoul(argv[5]), std::stoul(argv[6]));
+	if (argc == 7)
+	{
+		Character player1 = Character(argv[1], std::stoul(argv[2]), std::stoul(argv[3]));
+		Character player2 = Character(argv[4], std::stoul(argv[5]), std::stoul(argv[6]));
 
-        bool firstsTurn = true;
-        while (player1.getHP() > 0 && player2.getHP() > 0)
-        {
-            std::cout << player1.getName() << ": HP: " << player1.getHP() << ", DMG: " << player1.getDMG() << std::endl;
-            std::cout << player2.getName() << ": HP: " << player2.getHP() << ", DMG: " << player2.getDMG() << std::endl;
+		bool firstsTurn = true;
+		while (player1.getHP() > 0 && player2.getHP() > 0)
+		{
+			std::cout << player1.getName() << ": HP: " << player1.getHP() << ", DMG: " << player1.getDMG() << std::endl;
+			std::cout << player2.getName() << ": HP: " << player2.getHP() << ", DMG: " << player2.getDMG() << std::endl;
 
-            if (firstsTurn)
-            {
-                std::cout << player1.getName() << " -> " << player2.getName() << std::endl;
-                player2.gotHit(player1.getDMG());
-            }
-            else
-            {
-                std::cout << player2.getName() << " -> " << player1.getName() << std::endl;
-                player1.gotHit(player2.getDMG());
-            }
-            firstsTurn = !firstsTurn;
-        }
+			if (firstsTurn)
+			{
+				std::cout << player1.getName() << " -> " << player2.getName() << std::endl;
+				player2.gotHit(player1.getDMG());
+			}
+			else
+			{
+				std::cout << player2.getName() << " -> " << player1.getName() << std::endl;
+				player1.gotHit(player2.getDMG());
+			}
+			firstsTurn = !firstsTurn;
+		}
 
-        std::cout << player1.getName() << ": HP: " << player1.getHP() << ", DMG: " << player1.getDMG() << std::endl;
-        std::cout << player2.getName() << ": HP: " << player2.getHP() << ", DMG: " << player2.getDMG() << std::endl;
+		std::cout << player1.getName() << ": HP: " << player1.getHP() << ", DMG: " << player1.getDMG() << std::endl;
+		std::cout << player2.getName() << ": HP: " << player2.getHP() << ", DMG: " << player2.getDMG() << std::endl;
 
-        if (player1.getHP() == 0)
-        {
-            std::cout << player1.getName() << " died. " << player2.getName() << " wins." << std::endl;
-        }
-        else
-        {
-            std::cout << player2.getName() << " died. " << player1.getName() << " wins." << std::endl;
-            }
-        }
+		if (player1.getHP() == 0)
+		{
+			std::cout << player2.getName() << " wins. Remaining HP: " << player2.getHP() << std::endl;
+		}
+		else
+		{
+			std::cout << player1.getName() << " wins. Remaining HP: " << player1.getHP() << std::endl;
+		}
+	}
     else
     {
         std::cout << "Incorrect number of arguments!" << std::endl;
