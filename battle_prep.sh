e4c296c1cfefc53ee68e91714bbcdcee016a25c6
gboldi19 <55978636+gboldi19@users.noreply.github.com>
2020-09-26 01:53:53 +0200

    Checkin output existence

--- battle_prep.sh
+++ battle_prep.sh
@@ -2,11 +2,15 @@
 
 xIFS=$IFS
 IFS=$'\n'
-rm output.csv
+f_tocheck='output.csv'
 
-./a.out units/Unit1.json units/Unit2.json >> output.csv
-./a.out units/Unit1.json units/Unit3.json >> output.csv
-./a.out units/Unit2.json units/Unit1.json >> output.csv
-./a.out units/Unit2.json units/Unit3.json >> output.csv
-./a.out units/Unit3.json units/Unit2.json >> output.csv
-./a.out units/Unit3.json units/Unit1.json >> output.csv
+if [ -f $f_tocheck]; then
+    rm $f_tocheck
+fi
+
+./a.out 'units/Unit1.json' 'units/Unit2.json' >> $f_tocheck
+./a.out 'units/Unit1.json' 'units/Unit3.json' >> $f_tocheck
+./a.out 'units/Unit2.json' 'units/Unit1.json' >> $f_tocheck
+./a.out 'units/Unit2.json' 'units/Unit3.json' >> $f_tocheck
+./a.out 'units/Unit3.json' 'units/Unit2.json' >> $f_tocheck
+./a.out 'units/Unit3.json' 'units/Unit1.json' >> $f_tocheck
