fc48bb97c34cd73f1f43f87b9087fae1a8c45c59
gboldi19 <55978636+gboldi19@users.noreply.github.com>
2020-09-25 05:07:37 +0200

    Reverting a change

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@ int main (const int argc, const char* argv[])
 	{
 		std::vector<std::string> characterAttributes = Character::parseUnit(argv[1]);
 		Character player1 = Character(characterAttributes.at(0), stoul(characterAttributes.at(1)), stoul(characterAttributes.at(2)));
-
-		std::vector<std::string> characterAttributes = Character::parseUnit(argv[2]);
+		characterAttributes = Character::parseUnit(argv[2]);
 		Character player2 = Character(characterAttributes.at(0), stoul(characterAttributes.at(1)), stoul(characterAttributes.at(2)));
 
 		if (player1.getHP() == 0)
