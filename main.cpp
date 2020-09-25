1ca11ebc354eace018f6ceb738acf30aac30c16b
gboldi19 <55978636+gboldi19@users.noreply.github.com>
2020-09-25 04:46:15 +0200

    Removed unnecessary comments, methods, functions

--- main.cpp
+++ main.cpp
@@ -7,30 +7,9 @@ int main (const int argc, const char* argv[])
 	{
 		std::vector<std::string> characterAttributes = Character::parseUnit(argv[1]);
 		Character player1 = Character(characterAttributes.at(0), stoul(characterAttributes.at(1)), stoul(characterAttributes.at(2)));
-		characterAttributes = Character::parseUnit(argv[2]);
+
+		std::vector<std::string> characterAttributes = Character::parseUnit(argv[2]);
 		Character player2 = Character(characterAttributes.at(0), stoul(characterAttributes.at(1)), stoul(characterAttributes.at(2)));
-
-		bool firstsTurn = true;
-		while (player1.getHP() > 0 && player2.getHP() > 0)
-		{
-			std::cout << player1.getName() << ": HP: " << player1.getHP() << ", DMG: " << player1.getDMG() << std::endl;
-			std::cout << player2.getName() << ": HP: " << player2.getHP() << ", DMG: " << player2.getDMG() << std::endl;
-
-			if (firstsTurn)
-			{
-				std::cout << player1.getName() << " -> " << player2.getName() << std::endl;
-				player2.gotHit(player1.getDMG());
-			}
-			else
-			{
-				std::cout << player2.getName() << " -> " << player1.getName() << std::endl;
-				player1.gotHit(player2.getDMG());
-			}
-			firstsTurn = !firstsTurn;
-		}
-
-		std::cout << player1.getName() << ": HP: " << player1.getHP() << ", DMG: " << player1.getDMG() << std::endl;
-		std::cout << player2.getName() << ": HP: " << player2.getHP() << ", DMG: " << player2.getDMG() << std::endl;
 
 		if (player1.getHP() == 0)
 		{
@@ -43,7 +22,6 @@ int main (const int argc, const char* argv[])
 	}
 	else
 	{
-		//later to be adjusted !!!!!!!!!!!!!
 		throw std::runtime_error("Incorrect number of arguments!");
 	}
     return 0;
