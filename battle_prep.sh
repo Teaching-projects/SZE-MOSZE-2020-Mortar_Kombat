697d20a7eb92c0cb143b5d0085a6d789b54946f2
gboldi19 <55978636+gboldi19@users.noreply.github.com>
2020-09-25 20:47:17 +0200

    Reverting a change

--- battle_prep.sh
+++ battle_prep.sh
@@ -4,9 +4,9 @@ xIFS=$IFS
 IFS=$'\n'
 rm output.csv
 
-a.out units/Unit1.json units/Unit2.json >> output.csv
-a.out units/Unit1.json units/Unit3.json >> output.csv
-a.out units/Unit2.json units/Unit1.json >> output.csv
-a.out units/Unit2.json units/Unit3.json >> output.csv
-a.out units/Unit3.json units/Unit2.json >> output.csv
-a.out units/Unit3.json units/Unit1.json >> output.csv
+./a.out units/Unit1.json units/Unit2.json >> output.csv
+./a.out units/Unit1.json units/Unit3.json >> output.csv
+./a.out units/Unit2.json units/Unit1.json >> output.csv
+./a.out units/Unit2.json units/Unit3.json >> output.csv
+./a.out units/Unit3.json units/Unit2.json >> output.csv
+./a.out units/Unit3.json units/Unit1.json >> output.csv
